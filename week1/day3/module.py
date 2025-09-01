d1fcd4adb6cb229719acb25b3db89fd51a14f168
Yahya-Riswan <pk.yahyariswan@gmail.com>
2025-09-01 14:46:23 +0530

    Python Week 1

---
+++ week1/day3/module.py
@@ -0,0 +1,3 @@
+
+def area_of_circle(raduis):
+    return 3.14 * (raduis**2)
---
+++ week1/day3/task4.py
@@ -0,0 +1,5 @@
+import module
+
+r = float(input("Enter The Raduis Of The Circle : "));
+
+print(f"The Area Is {module.area_of_circle(r)}cm2")
